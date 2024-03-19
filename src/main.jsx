a511caa5edc4b847d745449c4c361933a2a57436
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2024-03-19 17:55:38 +0200

    сommit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/NannyServices">
+    <BrowserRouter basename="Nanny_Services">
       <App />
     </BrowserRouter>
   </React.StrictMode>
