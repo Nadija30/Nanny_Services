fecbd7341b58d0b8876617c4338891c280d5f193
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2024-03-20 00:51:47 +0200

    Updates

---
+++ assets/RegisterPage-BFXDhoK2.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Dk9J6RZ6.js";const e=()=>r.jsx(r.Fragment,{children:r.jsx("h1",{children:"Tyт буде форма для реєстрації"})}),s=()=>r.jsx(e,{});export{s as default};
---
+++ assets/sprite-DqmbqUi1.js
@@ -0,0 +1,1 @@
+const s="/Nanny_Services/assets/sprite-BKg-ewep.svg";export{s};
