12dbb4b4191533f0606b148a7fd603326ac9fde3
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2024-03-30 23:27:06 +0200

    commit

--- src/components/CatalogList/CatalogList.jsx
+++ src/components/CatalogList/CatalogList.jsx
@@ -1,13 +1,24 @@
-/* eslint-disable react/prop-types */
+import { useState } from 'react';
 import { CatalogItem } from '../CatalogItem/CatalogItem';
 import css from './CatalogList.module.css';
 import { nanoid } from 'nanoid';
 export const CatalogList = ({ data, isOnFavPage }) => {
+  const [displayedItems, setDisplayedItems] = useState(3);
+  const loadMore = () => {
+    setDisplayedItems(displayedItems + 3); // Збільшуємо кількість відображених елементів на 3
+  };
   return (
-    <ul className={css.list}>
-      {data.map((one) => (
-        <CatalogItem one={one} key={nanoid()} isOnFavPage={isOnFavPage} />
-      ))}
-    </ul>
+    <div className={css.listWrap}>
+      <ul className={css.list}>
+        {data.slice(0, displayedItems).map((one) => (
+          <CatalogItem one={one} key={nanoid()} isOnFavPage={isOnFavPage} />
+        ))}
+      </ul>
+      {data.length > displayedItems && (
+        <button className={css.loadMoreButton} onClick={loadMore}>
+          Load more
+        </button>
+      )}
+    </div>
   );
 };
